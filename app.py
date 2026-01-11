497e2c2e918406b33c07f8aaa5b67a7bddcd21b1
techkamau1 <kamauthetech@gmail.com>
2026-01-11 00:35:47 -0900

    Fix Render port binding and start command

--- app.py
+++ app.py
@@ -317,7 +317,8 @@ def upload_cv():
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
 
 
